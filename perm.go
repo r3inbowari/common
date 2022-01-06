92fc284fd0ba7fee18410cf68a7f028c3429b074
r3inbowari <r3inbowari@gmail.com>
2022-01-07 03:40:14 +0800

    fix perm

--- perm.go
+++ perm.go
@@ -1,6 +1,10 @@
 package common
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	. "github.com/klauspost/cpuid/v2"
 	"github.com/sirupsen/logrus"
 	"net"
@@ -11,41 +15,46 @@ import (
 )
 
 type Perm struct {
-	Digests []string `json:"digests"`
 	PermOptions
 }
 
 type PermOptions struct {
 	Log         *logrus.Logger
 	CheckSource string
+	AppId       string
 }
 
-func InitPerm(opt PermOptions) *Perm {
+func InitPermClient(opt PermOptions) *Perm {
 	var perm Perm
 	perm.PermOptions = opt
 	if perm.Log == nil {
 		perm.Log = logrus.New()
 	}
 	if opt.CheckSource == "" {
-		perm.Log.Warn("perm check source not set")
+		perm.Log.Warn("[PERM] check source not set")
 	}
+	if opt.AppId == "" {
+		perm.Log.Warn("[PERM] appid not set")
+	}
+	perm.Log.Info("[PERM] plugins loaded")
 	return &perm
 }
 
 func (p *Perm) Confirm() {
-	id := GetID()
-	if p.Digests == nil {
-		p.Log.Error("[SYS] permissions list not found, plz check your network")
-		exitOops()
+	id := GetID(p.AppId)
+
+	ok, err := TransportPerm(fmt.Sprintf("%s/verify/%s", p.CheckSource, id))
+	if err != nil {
+		p.Log.WithField("err", err.Error()).Error("[PERM] some error happened, plz report it to the developer, thanks!")
 	}
-	for _, v := range p.Digests {
-		if id == v {
-			p.Log.Infof("[SYS] permissions key -> %s [verified]", id)
-			return
-		}
+
+	if ok {
+		p.Log.Infof("[PERM] permissions key -> %s [verified]", id)
+		return
 	}
-	p.Log.Warn("[SYS] permissions key -> %s [unverified]", id)
-	p.Log.Warn("[SYS] oops, you don't have permission. plz contact the developer (⑉･̆-･̆⑉)")
+
+	p.Log.Warnf("[PERM] permissions key -> %s [unverified]", id)
+	p.Log.Warn("[PERM] oops, you don't have permission. plz contact the developer (⑉･̆-･̆⑉)")
 	exitOops()
 }
 
@@ -54,10 +63,10 @@ func exitOops() {
 	os.Exit(999)
 }
 
-func GetID() string {
+func GetID(appid string) string {
 	interfaces, err := net.Interfaces()
 	if err != nil {
-		panic("[SYS] panic" + err.Error())
+		panic("[PERM] not supported current operating system")
 	}
 	add := ""
 	for _, inter := range interfaces {
@@ -66,6 +75,7 @@ func GetID() string {
 		}
 	}
 	add += mixCPUInfo()
+	add += appid
 	return GetMD5(add)
 }
 
@@ -74,4 +84,40 @@ func mixCPUInfo() string {
 	r := CPU.PhysicalCores + CPU.ThreadsPerCore + CPU.LogicalCores + CPU.Family + CPU.Model + CPU.CacheLine + CPU.Cache.L1D*CPU.Cache.L1I
 	t := strconv.Itoa(r<<3 + int(CPU.VendorID))
 	return GetMD5(v + t)
+}
+
+type RequestResult struct {
+	Total   int          `json:"total"`
+	Data    RSATransport `json:"data"`
+	Code    int          `json:"code"`
+	Message string       `json:"msg"`
+}
+
+func TransportPerm(url string) (bool, error) {
+	pair, err := CreatePair()
+	if err != nil {
+		return false, errors.New("create pair failed")
+	}
+
+	rt := *pair.NewRSATransport()
+	rtm, err := json.Marshal(rt)
+	if err != nil {
+		return false, errors.New("create transport failed")
+	}
+
+	var rtr RequestResult
+
+	_, err = RequestJson(RequestOptions{
+		Url:    url,
+		Reader: bytes.NewBuffer(rtm),
+	}, &rtr)
+	if err != nil {
+		return false, err
+	}
+
+	d, err := rtr.Data.Decrypt(pair.Private)
+	if err != nil {
+		return false, errors.New("decrypt failed")
+	}
+	return string(d) == "!*#34787894@qq.com#*b41b4af78240ae375a4cb0b95932ffc3", nil
 }
--- test/perm_test.go
+++ test/perm_test.go
@@ -6,7 +6,6 @@ import (
 )
 
 func TestPerm(t *testing.T) {
-	p := common.InitPerm(common.PermOptions{})
+	p := common.InitPermClient(common.PermOptions{CheckSource: "http://127.0.0.1:18833", AppId: "acd3f8c51a"})
 	p.Confirm()
-
 }
